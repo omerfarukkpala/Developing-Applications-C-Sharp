14b98f829f32dd417ad364f258778da869102cf6
= <omerfarukkpala@gmail.com>
2023-11-08 12:05:51 +0300

    number added.

---
+++ Int Variables/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Int_Variables
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*int number;
+            number = Convert.ToInt32(textBox1.Text);
+            label2.Text = number.ToString();*/
+
+            int number, cube;
+            number = Convert.ToInt32(textBox1.Text);
+            cube = number * number * number;
+            label2.Text = cube.ToString();
+
+        }
+    }
+}
--- Int Variables/Program.cs
+++ Int Variables/Program.cs
@@ -16,7 +16,7 @@ namespace Int_Variables
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
