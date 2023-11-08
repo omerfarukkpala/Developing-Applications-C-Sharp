200685f87e43e27dfc5a6fd5f0e2a94a95fbd877
= <omerfarukkpala@gmail.com>
2023-11-08 12:19:55 +0300

    "Arithmetic operations for the four basic operations have been performed."

---
+++ Int Variables/Form3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Int_Variables
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1, number2, sum, difference, product, quotient;
+            number1 = Convert.ToInt32(textBox1.Text);
+            number2 = Convert.ToInt32(textBox2.Text);
+            sum = number1 + number2;
+            difference = number1 - number2;
+            product = number1 * number2;
+            quotient = number1 / number2;
+            MessageBox.Show("Sum: " + sum + "\nDifference: " + difference + "\nProduct: " + product + "\nQuotient: " + quotient);
+
+        }
+    }
+}
