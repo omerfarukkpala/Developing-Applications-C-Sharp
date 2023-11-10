9d4a83b72e3669d430b578a2b730301c4d3b5aae
= <omerfarukkpala@gmail.com>
2023-11-10 18:09:04 +0300

    progress bar

---
+++ Loops/Form8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 75;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 75;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value -= 10;
+
+        }
+    }
+}
