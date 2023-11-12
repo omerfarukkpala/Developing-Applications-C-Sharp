b4599ce6b8e86a2c5616d16b462b93c27b869419
= <omerfarukkpala@gmail.com>
2023-11-12 13:12:03 +0300

    Send 10 elements in the array, list numbers greater than 10 and even

--- Arrays/Form1.cs
+++ Arrays/Form1.cs
@@ -27,12 +27,41 @@ namespace Arrays
             //label1.Text = numbers[5].ToString();
 
             // Create 5 arrays with decimal values.
-            double[] decimalNumbers = { 5.12, 3.24, 7.81, 4.17, 9.16 };
+            double[] decimalNumbers = { 1.22, 3.84, 9.81, 2.47, 5.86 };
             label1.Text = decimalNumbers[2].ToString();
 
             // Create 10 arrays using char.
             //char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
             //label1.Text = letters[3].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //string[] cities = { "Bursa", "Amasya", "Kocaeli", "Urfa", "Afyon", "Antep" };
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    listBox1.Items.Add(cities[i]);
+            //}
+
+            //int[] numbers = { 4, 2, 3, 1, 5, 6, 7, 9 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        listBox1.Items.Add(numbers[i]);
+            //    }
+            //}
+
+            // Send 10 elements in the array, list numbers greater than 10 and even.
+            int[] numbers = { 5, 12, 4, 7, 9, 30, 23, 6, 10, 21 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 10 && numbers[i] % 2 == 0)
+                {
+                    listBox1.Items.Add(numbers[i]);
+                }
+            }
         }
     }
 }
