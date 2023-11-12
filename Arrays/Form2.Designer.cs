aeaa6acbe8df925f86103c59c71dafc60ea1d1b8
= <omerfarukkpala@gmail.com>
2023-11-12 13:27:04 +0300

    foreach completed.

--- Arrays/Form2.Designer.cs
+++ Arrays/Form2.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 238);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(503, 379);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Foreach";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Arrays/Form2.cs
+++ Arrays/Form2.cs
@@ -16,5 +16,47 @@ namespace Arrays
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string[] people = { "Sercan", "Ahmet", "Yusuf", "Ayşe", "Fatma", "Ali" };
+            //foreach (string person in people)
+            //{
+            //    listBox1.Items.Add(person);  
+            //}
+
+            //int total = 0;
+            //int[] exams = { 70, 65, 85, 100, 90 };
+            //foreach (int score in exams)
+            //{
+            //    listBox1.Items.Add(score);
+            //    total += score;
+            //}
+            //label1.Text = total.ToString();
+
+            //Calculate the average of exam scores
+            //int average = total / exams.Length;
+            //label2.Text = average.ToString();
+
+            // In a 10-element integer array, list the numbers divisible by 4 using a foreach loop.
+            // For the same question, this time, list how many numbers are divisible by 4.
+            // For the same question, this time, also calculate the sum of numbers divisible by 4.
+
+            int[] numbers = { 5, 7, 12, 4, 11, 16, 8, 1, 13, 2 };
+            int count = 0;
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 4 == 0)
+                {
+                    count++;
+                    listBox1.Items.Add(number);
+                    sum += number;
+                }
+            }
+            label1.Text = count.ToString();
+            label2.Text = sum.ToString();
+
+        }
     }
 }
