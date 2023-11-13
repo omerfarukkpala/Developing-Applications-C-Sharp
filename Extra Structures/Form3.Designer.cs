9a7d390d54a6d1ab16b02e2d9a747222099ea746
= <omerfarukkpala@gmail.com>
2023-11-13 23:02:51 +0300

    homework completed.

--- Extra Structures/Form3.Designer.cs
+++ Extra Structures/Form3.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Extra_Structures
 {
-    partial class Form2
+    partial class Form3
     {
         /// <summary>
         /// Required designer variable.
@@ -30,39 +30,54 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(38, 39);
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(50, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 48);
-            this.label1.TabIndex = 3;
+            this.label1.TabIndex = 5;
             this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 116);
+            this.button1.ForeColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(34, 103);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 48);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form2
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(283, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 48);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(672, 379);
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.ForeColor = System.Drawing.Color.Coral;
-            this.Name = "Form2";
-            this.Text = "Captcha";
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "Form3";
+            this.Text = "Form3";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +87,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
---
+++ Extra Structures/Form3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Extra_Structures
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                string[] symbolSet1 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+               "T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+               "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","-","*","/","!",
+               "'","#","$","%","₺","€"};
+                string[] symbolSet2 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+               "T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+               "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","-","*","/","!",
+               "'","#","$","%","₺","€"};
+                string[] symbolSet3 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+               "T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+               "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","-","*","/","!",
+               "'","#","$","%","₺","€"};
+                string[] symbolSet4 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+               "T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+               "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","-","*","/","!",
+               "'","#","$","%","₺","€"};
+                string[] symbolSet5 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+               "T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+               "r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","-","*","/","!",
+               "'","#","$","%","₺","€"};
+
+                Random random = new Random();
+                int index1 = random.Next(0, symbolSet1.Length);
+                int index2 = random.Next(0, symbolSet2.Length);
+                int index3 = random.Next(0, symbolSet3.Length);
+                int index4 = random.Next(0, symbolSet4.Length);
+                int index5 = random.Next(0, symbolSet5.Length);
+
+            label1.Text = symbolSet1[index1] + symbolSet2[index2] + symbolSet3[index3] +
+               symbolSet4[index4] + symbolSet5[index5];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
