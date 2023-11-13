a1f9e5c66725f06ab217d468b7d9f4a0c6298042
= <omerfarukkpala@gmail.com>
2023-11-13 21:02:48 +0300

    random

--- Extra Structures/Form1.Designer.cs
+++ Extra Structures/Form1.Designer.cs
@@ -105,18 +105,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(130, 155);
+            this.button1.Location = new System.Drawing.Point(158, 135);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 44);
             this.button1.TabIndex = 11;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 415);
+            this.ClientSize = new System.Drawing.Size(487, 295);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
--- Extra Structures/Form1.cs
+++ Extra Structures/Form1.cs
@@ -16,5 +16,22 @@ namespace Extra_Structures
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Random rnd = new Random();
+            //int number;
+            //number = rnd.Next(1, 10);
+            //label1.Text = number.ToString();
+
+            //Random rd = new Random();
+            //int s1, s2, s3;
+            //s1 = rd.Next(1, 5);
+            //s2 = rd.Next(1, 5);
+            //s3 = rd.Next(1, 5);
+            //label1.Text = s1.ToString();
+            //label2.Text = s2.ToString();
+            //label3.Text = s3.ToString();
+        }
     }
 }
