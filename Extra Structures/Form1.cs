91ed8c110a4ef60ac87b2bb2521c9ef6d702c4ff
= <omerfarukkpala@gmail.com>
2023-11-13 22:07:12 +0300

    extra structures completed.

--- Extra Structures/Form1.cs
+++ Extra Structures/Form1.cs
@@ -32,6 +32,60 @@ namespace Extra_Structures
             //label1.Text = s1.ToString();
             //label2.Text = s2.ToString();
             //label3.Text = s3.ToString();
+
+            Random random = new Random();
+            int number1, number2, number3, number4;
+            number1 = random.Next(1, 5);
+            number2 = random.Next(1, 5);
+            number3 = random.Next(1, 5);
+            number4 = random.Next(1, 5);
+            label1.Text = number1.ToString();
+            label2.Text = number2.ToString();
+            label3.Text = number3.ToString();
+            label4.Text = number4.ToString();
+
+            // Number 1
+            if (textBox1.Text == label1.Text)
+            {
+                textBox1.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+            }
+
+            // Number 2
+            if (textBox2.Text == label2.Text)
+            {
+                textBox2.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+            }
+
+            // Number 3
+            if (textBox3.Text == label3.Text)
+            {
+                textBox3.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+
+            // Number 4
+            if (textBox4.Text == label4.Text)
+            {
+                textBox4.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox4.BackColor = Color.Red;
+            }
+
+
+
         }
     }
 }
