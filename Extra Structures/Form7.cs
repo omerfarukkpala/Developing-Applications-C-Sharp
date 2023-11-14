69e6020e731b9f26f69bd48a2f27228e4a870413
= <omerfarukkpala@gmail.com>
2023-11-14 10:38:52 +0300

    point completed.

---
+++ Extra Structures/Form7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Extra_Structures
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point location = new Point();
+            location.X = 300;
+            location.Y = 200;
+            pictureBox1.Location = location;
+        }
+    }
+}
