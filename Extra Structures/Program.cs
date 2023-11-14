6f7f711317eeabcb820a4e8dd307348b3c9af74f
= <omerfarukkpala@gmail.com>
2023-11-14 10:43:42 +0300

    enum

--- Extra Structures/Program.cs
+++ Extra Structures/Program.cs
@@ -16,7 +16,7 @@ namespace Extra_Structures
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form7());
+            Application.Run(new Form8());
         }
     }
 }
