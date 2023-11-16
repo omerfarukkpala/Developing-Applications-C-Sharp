aa64a00cde5cce4253419e142e4c332cb00a75c0
= <omerfarukkpala@gmail.com>
2023-11-16 14:31:57 +0300

    FormLogin

--- Personnel Management System/FormLogin.cs
+++ Personnel Management System/FormLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,8 @@ namespace Personnel_Management_System
         {
             InitializeComponent();
         }
+
+        SqlConnection connection = new SqlConnection("Data Source=OMERFARUKPALA;Initial Catalog=PersonelDatabase;Integrated Security=True");
 
         private void BtnGirisYap_Click(object sender, EventArgs e)
         {
--- Personnel Management System/Program.cs
+++ Personnel Management System/Program.cs
@@ -16,7 +16,7 @@ namespace Personnel_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMainForm());
+            Application.Run(new FormLogin());
         }
     }
 }
