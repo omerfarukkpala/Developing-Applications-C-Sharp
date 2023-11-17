f505462d477b3e7b4d5ea85c98ce9167e33505b4
= <omerfarukkpala@gmail.com>
2023-11-17 16:24:25 +0300

    completed.

--- File Operations/Form3.Designer.cs
+++ File Operations/Form3.Designer.cs
@@ -63,7 +63,7 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "s";
             this.ResumeLayout(false);
 
         }
