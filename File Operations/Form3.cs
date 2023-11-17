b51e2b803be169c7c5523ffee6b36a0e30088d6d
= <omerfarukkpala@gmail.com>
2023-11-17 11:32:47 +0300

    Continued

---
+++ File Operations/Form3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Operations
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                {
+                    string line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        listBox1.Items.Add(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
