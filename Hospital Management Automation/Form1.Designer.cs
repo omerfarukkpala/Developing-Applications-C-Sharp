e36cb8fae0e59db3da8b4fbd036de80ab5733e8e
= <omerfarukkpala@gmail.com>
2023-11-17 16:43:53 +0300

    PALA Love  Hospital

--- Hospital Management Automation/Form1.Designer.cs
+++ Hospital Management Automation/Form1.Designer.cs
@@ -28,13 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnSekreterGirisi = new System.Windows.Forms.Button();
+            this.BtnDoktorGirisi = new System.Windows.Forms.Button();
+            this.BtnHastaGirisi = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(67, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(318, 44);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "PALA Love  Hospital";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(409, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(228, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(517, 304);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Secretary";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Doctor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Patient";
+            // 
+            // BtnSekreterGirisi
+            // 
+            this.BtnSekreterGirisi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnSekreterGirisi.BackgroundImage")));
+            this.BtnSekreterGirisi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnSekreterGirisi.Location = new System.Drawing.Point(462, 188);
+            this.BtnSekreterGirisi.Name = "BtnSekreterGirisi";
+            this.BtnSekreterGirisi.Size = new System.Drawing.Size(176, 113);
+            this.BtnSekreterGirisi.TabIndex = 10;
+            this.BtnSekreterGirisi.UseVisualStyleBackColor = true;
+            // 
+            // BtnDoktorGirisi
+            // 
+            this.BtnDoktorGirisi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnDoktorGirisi.BackgroundImage")));
+            this.BtnDoktorGirisi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnDoktorGirisi.Location = new System.Drawing.Point(248, 188);
+            this.BtnDoktorGirisi.Name = "BtnDoktorGirisi";
+            this.BtnDoktorGirisi.Size = new System.Drawing.Size(176, 113);
+            this.BtnDoktorGirisi.TabIndex = 9;
+            this.BtnDoktorGirisi.UseVisualStyleBackColor = true;
+            // 
+            // BtnHastaGirisi
+            // 
+            this.BtnHastaGirisi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnHastaGirisi.BackgroundImage")));
+            this.BtnHastaGirisi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnHastaGirisi.Location = new System.Drawing.Point(37, 188);
+            this.BtnHastaGirisi.Name = "BtnHastaGirisi";
+            this.BtnHastaGirisi.Size = new System.Drawing.Size(176, 113);
+            this.BtnHastaGirisi.TabIndex = 8;
+            this.BtnHastaGirisi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(686, 361);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnSekreterGirisi);
+            this.Controls.Add(this.BtnDoktorGirisi);
+            this.Controls.Add(this.BtnHastaGirisi);
+            this.Name = "Form1";
+            this.Text = "PALA Love  Hospital";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnSekreterGirisi;
+        private System.Windows.Forms.Button BtnDoktorGirisi;
+        private System.Windows.Forms.Button BtnHastaGirisi;
     }
 }
 
