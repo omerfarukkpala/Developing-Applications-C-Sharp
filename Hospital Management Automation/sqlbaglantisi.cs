0c367ee6332067d0fd370e9ab9c144eb1a199d9b
= <omerfarukkpala@gmail.com>
2023-11-18 14:48:36 +0300

    connection

---
+++ Hospital Management Automation/sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_Automation
+{
+    internal class sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+        SqlConnection baglan = new SqlConnection("Data Source=OMERFARUKPALA;Initial Catalog=HastaneProje;Integrated Security=True");
+        baglan.Open();
+        return baglan;
+        }
+    }
+}
