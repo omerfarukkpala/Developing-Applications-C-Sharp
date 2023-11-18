33388c8728a9de86aa165ed33120dd24148f71b5
= <omerfarukkpala@gmail.com>
2023-11-18 14:33:56 +0300

    Department form added.

---
+++ Hospital Management Automation/DepartmentForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Hospital_Management_Automation
+{
+    partial class DepartmentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TxtBrans = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txtid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.Color.Lime;
+            this.BtnGuncelle.Location = new System.Drawing.Point(153, 175);
+            this.BtnGuncelle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(204, 50);
+            this.BtnGuncelle.TabIndex = 48;
+            this.BtnGuncelle.Text = "Update";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnSil.Location = new System.Drawing.Point(269, 117);
+            this.BtnSil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(88, 50);
+            this.BtnSil.TabIndex = 47;
+            this.BtnSil.Text = "Delete";
+            this.BtnSil.UseVisualStyleBackColor = false;
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnEkle.Location = new System.Drawing.Point(153, 117);
+            this.BtnEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(88, 50);
+            this.BtnEkle.TabIndex = 46;
+            this.BtnEkle.Text = "Add";
+            this.BtnEkle.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(380, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(630, 215);
+            this.dataGridView1.TabIndex = 45;
+            // 
+            // TxtBrans
+            // 
+            this.TxtBrans.Location = new System.Drawing.Point(153, 57);
+            this.TxtBrans.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtBrans.Name = "TxtBrans";
+            this.TxtBrans.Size = new System.Drawing.Size(202, 24);
+            this.TxtBrans.TabIndex = 44;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 18);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Department  Name :";
+            // 
+            // Txtid
+            // 
+            this.Txtid.Location = new System.Drawing.Point(153, 6);
+            this.Txtid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txtid.Name = "Txtid";
+            this.Txtid.Size = new System.Drawing.Size(202, 24);
+            this.Txtid.TabIndex = 42;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 18);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Department  ID :";
+            // 
+            // DepartmentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 247);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TxtBrans);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txtid);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DepartmentForm";
+            this.Text = "Department Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TxtBrans;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txtid;
+        private System.Windows.Forms.Label label1;
+    }
+}
