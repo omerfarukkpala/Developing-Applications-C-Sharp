e832743bba7d151d6ab4919cfe7d32a23db21042
= <omerfarukkpala@gmail.com>
2023-11-18 11:03:17 +0300

    PatientRegistrationForm form completed.

--- Hospital Management Automation/PatientRegistrationForm.Designer.cs
+++ Hospital Management Automation/PatientRegistrationForm.Designer.cs
@@ -28,12 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnKayitYap = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CmbCinsiyet = new System.Windows.Forms.ComboBox();
+            this.MskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnKayitYap
+            // 
+            this.BtnKayitYap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnKayitYap.Location = new System.Drawing.Point(271, 450);
+            this.BtnKayitYap.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.BtnKayitYap.Name = "BtnKayitYap";
+            this.BtnKayitYap.Size = new System.Drawing.Size(280, 70);
+            this.BtnKayitYap.TabIndex = 24;
+            this.BtnKayitYap.Text = "Log in";
+            this.BtnKayitYap.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(121, 389);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 24);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Gender :";
+            // 
+            // CmbCinsiyet
+            // 
+            this.CmbCinsiyet.FormattingEnabled = true;
+            this.CmbCinsiyet.Items.AddRange(new object[] {
+            "Erkek",
+            "Kadın"});
+            this.CmbCinsiyet.Location = new System.Drawing.Point(271, 381);
+            this.CmbCinsiyet.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.CmbCinsiyet.Name = "CmbCinsiyet";
+            this.CmbCinsiyet.Size = new System.Drawing.Size(278, 32);
+            this.CmbCinsiyet.TabIndex = 22;
+            // 
+            // MskTelefon
+            // 
+            this.MskTelefon.Location = new System.Drawing.Point(271, 242);
+            this.MskTelefon.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MskTelefon.Mask = "(999) 000-0000";
+            this.MskTelefon.Name = "MskTelefon";
+            this.MskTelefon.Size = new System.Drawing.Size(278, 29);
+            this.MskTelefon.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(94, 247);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 24);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Telephone :";
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(271, 105);
+            this.TxtSoyad.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(278, 29);
+            this.TxtSoyad.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 110);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 24);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Surname :";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(271, 36);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(278, 29);
+            this.TxtAd.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Name :";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(271, 312);
+            this.TxtSifre.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(278, 29);
+            this.TxtSifre.TabIndex = 21;
+            this.TxtSifre.UseSystemPasswordChar = true;
+            // 
+            // MskTC
+            // 
+            this.MskTC.Location = new System.Drawing.Point(271, 172);
+            this.MskTC.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(278, 29);
+            this.MskTC.TabIndex = 19;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 317);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 24);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Password :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 177);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "TR ID No :";
+            // 
+            // PatientRegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(861, 676);
+            this.Controls.Add(this.BtnKayitYap);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.CmbCinsiyet);
+            this.Controls.Add(this.MskTelefon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "PatientRegistrationForm";
             this.Text = "PatientRegistrationForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnKayitYap;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox CmbCinsiyet;
+        private System.Windows.Forms.MaskedTextBox MskTelefon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
