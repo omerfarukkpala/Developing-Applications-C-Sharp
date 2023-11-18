b092ddcd504cca8d8b371aa8544894262de4313f
= <omerfarukkpala@gmail.com>
2023-11-18 11:40:28 +0300

    Edit Information Form updated.

---
+++ Hospital Management Automation/EditInformationForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Hospital_Management_Automation
+{
+    partial class EditInformationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnBilgiGuncelle = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CmbCinsiyet = new System.Windows.Forms.ComboBox();
+            this.MskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnBilgiGuncelle
+            // 
+            this.BtnBilgiGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnBilgiGuncelle.Location = new System.Drawing.Point(220, 447);
+            this.BtnBilgiGuncelle.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.BtnBilgiGuncelle.Name = "BtnBilgiGuncelle";
+            this.BtnBilgiGuncelle.Size = new System.Drawing.Size(344, 72);
+            this.BtnBilgiGuncelle.TabIndex = 37;
+            this.BtnBilgiGuncelle.Text = "Update";
+            this.BtnBilgiGuncelle.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 383);
+            this.label6.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 18);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "Gender :";
+            // 
+            // CmbCinsiyet
+            // 
+            this.CmbCinsiyet.FormattingEnabled = true;
+            this.CmbCinsiyet.Items.AddRange(new object[] {
+            "Erkek",
+            "Kadın"});
+            this.CmbCinsiyet.Location = new System.Drawing.Point(220, 375);
+            this.CmbCinsiyet.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.CmbCinsiyet.Name = "CmbCinsiyet";
+            this.CmbCinsiyet.Size = new System.Drawing.Size(340, 26);
+            this.CmbCinsiyet.TabIndex = 35;
+            // 
+            // MskTelefon
+            // 
+            this.MskTelefon.Location = new System.Drawing.Point(220, 231);
+            this.MskTelefon.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.MskTelefon.Mask = "(999) 000-0000";
+            this.MskTelefon.Name = "MskTelefon";
+            this.MskTelefon.Size = new System.Drawing.Size(340, 24);
+            this.MskTelefon.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 234);
+            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 18);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Telephone :";
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(220, 88);
+            this.TxtSoyad.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(340, 24);
+            this.TxtSoyad.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 92);
+            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 18);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Surname :";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(220, 16);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(340, 24);
+            this.TxtAd.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Name :";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(220, 303);
+            this.TxtSifre.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(340, 24);
+            this.TxtSifre.TabIndex = 34;
+            this.TxtSifre.UseSystemPasswordChar = true;
+            // 
+            // MskTC
+            // 
+            this.MskTC.Location = new System.Drawing.Point(220, 157);
+            this.MskTC.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(340, 24);
+            this.MskTC.TabIndex = 32;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 308);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 18);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Password :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 163);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 18);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "TR ID No :";
+            // 
+            // EditInformationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(642, 562);
+            this.Controls.Add(this.BtnBilgiGuncelle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.CmbCinsiyet);
+            this.Controls.Add(this.MskTelefon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "EditInformationForm";
+            this.Text = "Edit Information Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBilgiGuncelle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox CmbCinsiyet;
+        private System.Windows.Forms.MaskedTextBox MskTelefon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
