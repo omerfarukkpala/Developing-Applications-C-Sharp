1345ea0aaa3ff8e1dc1a92178ad1f3f72162ef5a
= <omerfarukkpala@gmail.com>
2023-11-29 10:10:05 +0300

    project examples completed.

--- ProjectExamples/Form2.Designer.cs
+++ ProjectExamples/Form2.Designer.cs
@@ -84,7 +84,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "Project Examples";
             this.ResumeLayout(false);
             this.PerformLayout();
 
