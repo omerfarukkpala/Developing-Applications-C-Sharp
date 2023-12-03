baa16e03d0e5097b3cf304b72f43bc8b85df546e
= <omerfarukkpala@gmail.com>
2023-12-03 22:07:20 +0300

    ..

--- Entity Project/FrmGiris.Designer.cs
+++ Entity Project/FrmGiris.Designer.cs
@@ -42,7 +42,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(178, 50);
             this.button1.TabIndex = 9;
-            this.button1.Text = "Giriş Yap";
+            this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
