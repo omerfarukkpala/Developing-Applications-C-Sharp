4ed47cde64bc1c9b8e9e9cfeda7e90510a7a5d63
= <omerfarukkpala@gmail.com>
2023-12-03 20:31:04 +0300

    .

--- N-tier Architecture/NKatmanliMimari/Form1.Designer.cs
+++ N-tier Architecture/NKatmanliMimari/Form1.Designer.cs
@@ -186,7 +186,7 @@
             // 
             // BtnGuncelle
             // 
-            this.BtnGuncelle.Location = new System.Drawing.Point(528, 165);
+            this.BtnGuncelle.Location = new System.Drawing.Point(688, 63);
             this.BtnGuncelle.Name = "BtnGuncelle";
             this.BtnGuncelle.Size = new System.Drawing.Size(154, 45);
             this.BtnGuncelle.TabIndex = 16;
