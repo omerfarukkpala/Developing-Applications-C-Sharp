88afdb5f1d5f290eb87a678052b46adba7a434ae
= <omerfarukkpala@gmail.com>
2023-12-04 21:13:27 +0300

    ..

--- Entity Project/Form1.Designer.cs
+++ Entity Project/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(435, 60);
+            this.dataGridView1.Location = new System.Drawing.Point(416, 60);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(747, 222);
