e5e3b550f950901a753cf4a8aae5f46c4d6907e7
= <omerfarukkpala@gmail.com>
2023-12-06 22:11:57 +0300

    ..

--- Entity Project/Form1.Designer.cs
+++ Entity Project/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(400, 60);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(747, 222);
+            this.dataGridView1.Size = new System.Drawing.Size(747, 184);
             this.dataGridView1.TabIndex = 17;
             // 
             // BtnGuncelle
