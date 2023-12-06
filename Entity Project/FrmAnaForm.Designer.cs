8cb057cf5db00b08876462e445c8884563217f62
= <omerfarukkpala@gmail.com>
2023-12-06 22:14:15 +0300

    .

--- Entity Project/FrmAnaForm.Designer.cs
+++ Entity Project/FrmAnaForm.Designer.cs
@@ -70,7 +70,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(905, 317);
+            this.ClientSize = new System.Drawing.Size(873, 283);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
