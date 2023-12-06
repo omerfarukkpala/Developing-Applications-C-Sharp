4bc82786d176d751a248a7320385f897737503b1
= <omerfarukkpala@gmail.com>
2023-12-06 22:13:59 +0300

    ..

--- Entity Project/FrmAnaForm.Designer.cs
+++ Entity Project/FrmAnaForm.Designer.cs
@@ -70,7 +70,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(970, 442);
+            this.ClientSize = new System.Drawing.Size(927, 362);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
