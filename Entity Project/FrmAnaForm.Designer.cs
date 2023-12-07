3ed7294f2a181b5f668bd062f2e3268ee5569a54
= <omerfarukkpala@gmail.com>
2023-12-07 22:35:07 +0300

    .

--- Entity Project/FrmAnaForm.Designer.cs
+++ Entity Project/FrmAnaForm.Designer.cs
@@ -70,7 +70,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 215);
+            this.ClientSize = new System.Drawing.Size(798, 204);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
