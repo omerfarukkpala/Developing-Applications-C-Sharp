8dd9a36e560c5a3fb079c94c5bd9a1a3b0d40269
= <omerfarukkpala@gmail.com>
2023-12-07 22:33:08 +0300

    .

--- Entity Project/FrmAnaForm.Designer.cs
+++ Entity Project/FrmAnaForm.Designer.cs
@@ -77,7 +77,6 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmAnaForm";
-            this.Text = "Frm Ana Form";
             this.ResumeLayout(false);
 
         }
