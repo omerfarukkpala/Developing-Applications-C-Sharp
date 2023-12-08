6b76701ccbff512d002b4907a41e9b309b40da1e
= <omerfarukkpala@gmail.com>
2023-12-08 22:01:24 +0300

    .

--- Entity Project/FrmUrun.Designer.cs
+++ Entity Project/FrmUrun.Designer.cs
@@ -62,7 +62,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 253);
+            this.dataGridView1.Location = new System.Drawing.Point(28, 261);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(540, 202);
             this.dataGridView1.TabIndex = 39;
