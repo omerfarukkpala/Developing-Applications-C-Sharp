d5f3d3656f56c4323a57bb816c7a58f89e89fc67
= <omerfarukkpala@gmail.com>
2023-12-12 21:58:35 +0300

    .

--- Entity Project/FrmUrun.Designer.cs
+++ Entity Project/FrmUrun.Designer.cs
@@ -138,9 +138,9 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(124, 200);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 18);
+            this.label6.Size = new System.Drawing.Size(93, 18);
             this.label6.TabIndex = 31;
-            this.label6.Text = "DURUM:";
+            this.label6.Text = "SITUATION :";
             // 
             // TxtFiyat
             // 
