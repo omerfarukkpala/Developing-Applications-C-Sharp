9de2e658ea8c047e9e769db796934fef21ac29d0
= <omerfarukkpala@gmail.com>
2023-12-14 18:44:01 +0300

    .

--- Entity Project/FrmUrun.Designer.cs
+++ Entity Project/FrmUrun.Designer.cs
@@ -226,7 +226,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(606, 499);
+            this.ClientSize = new System.Drawing.Size(592, 497);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.BtnTemizle);
