e46f0abf175bb9d76e5967ea4e9a4c772bee52fd
= <omerfarukkpala@gmail.com>
2023-12-16 18:30:25 +0300

    .

--- BonusProject/FrmOgrenciNotlar.Designer.cs
+++ BonusProject/FrmOgrenciNotlar.Designer.cs
@@ -38,14 +38,14 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.Size = new System.Drawing.Size(859, 472);
             this.dataGridView1.TabIndex = 1;
             // 
             // FrmOgrenciNotlar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(859, 472);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FrmOgrenciNotlar";
             this.Text = "Ogrenci Not Ekranı";
