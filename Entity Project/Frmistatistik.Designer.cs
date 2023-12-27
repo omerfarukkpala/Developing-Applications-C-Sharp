9a1b0986ac8388cadaa798f1dcd854ed32dd76c1
= <omerfarukkpala@gmail.com>
2023-12-27 15:15:04 +0300

    ...

--- Entity Project/Frmistatistik.Designer.cs
+++ Entity Project/Frmistatistik.Designer.cs
@@ -478,7 +478,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1404, 529);
+            this.ClientSize = new System.Drawing.Size(1392, 529);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel8);
             this.Controls.Add(this.panel9);
